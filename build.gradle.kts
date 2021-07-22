023cfb5bdd6c95678de7f677bab5ada339f7cb0f
ezura <yuka.ezura@gmail.com>
2021-07-22 21:12:04 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("org.jetbrains.intellij") version "1.1.4"
+    java
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "work.ezura"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2021.1.2"
+}
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
