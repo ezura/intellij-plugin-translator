cb3f40db373a8861cd5a8b8757ee1ee177fd2802
ezura <yuka.ezura@gmail.com>
2021-07-24 16:33:35 +0900

    Add enable condition

--- src/main/kotlin/TranslationAction.kt
+++ src/main/kotlin/TranslationAction.kt
@@ -5,7 +5,7 @@ class TranslationAction: AnAction() {
 
     override fun update(e: AnActionEvent) {
         super.update(e)
-        e.presentation.isEnabledAndVisible = true
+        e.presentation.isEnabledAndVisible = !e.presentation.text.isNullOrEmpty()
     }
 
     override fun actionPerformed(e: AnActionEvent) {
