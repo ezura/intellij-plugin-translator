a5c4803290ffa9cac547dd3bf5d9e4932e55bd13
ezura <yuka.ezura@gmail.com>
2021-07-24 15:13:45 +0900

    Set always valid

--- src/main/kotlin/TranslationAction.kt
+++ src/main/kotlin/TranslationAction.kt
@@ -3,6 +3,11 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 
 class TranslationAction: AnAction() {
 
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+        e.presentation.isEnabledAndVisible = true
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
         TODO("Not yet implemented")
     }
