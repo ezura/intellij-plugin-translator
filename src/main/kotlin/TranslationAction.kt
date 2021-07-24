982dfc5dc3261b72ff1ec76920a6800d2ee90504
ezura <yuka.ezura@gmail.com>
2021-07-24 14:53:33 +0900

    Add TranslationAction

---
+++ src/main/kotlin/TranslationAction.kt
@@ -0,0 +1,9 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class TranslationAction: AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
