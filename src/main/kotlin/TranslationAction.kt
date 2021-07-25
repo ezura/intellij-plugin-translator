48127362ac6d4abc1dae65144ac866bd758bcfe6
ezura <yuka.ezura@gmail.com>
2021-07-25 09:21:02 +0900

    Show balloon

--- src/main/kotlin/TranslationAction.kt
+++ src/main/kotlin/TranslationAction.kt
@@ -1,5 +1,10 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.ui.MessageType
+import com.intellij.openapi.ui.popup.Balloon
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.ui.awt.RelativePoint
 
 class TranslationAction: AnAction() {
 
@@ -12,6 +17,28 @@ class TranslationAction: AnAction() {
     }
 
     override fun actionPerformed(e: AnActionEvent) {
-        TODO("Not yet implemented")
+        val editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        val caret = editor.caretModel.primaryCaret
+        val visualPosition = caret.selectionEndPosition
+        // TODO: trim line break and "//"
+        val selectedText = caret.selectedText ?: ""
+        // TODO: Call a translate API
+        // TODO: Cancel the action if the selected text has been changed.
+        val actualPosition = editor.visualPositionToXY(visualPosition)
+            .also {
+                val editorContentPosition = editor.contentComponent.locationOnScreen
+                it.translate(
+                    editorContentPosition.x,
+                    editorContentPosition.y + editor.lineHeight
+                )
+            }
+
+        JBPopupFactory.getInstance()
+            .createHtmlTextBalloonBuilder(selectedText, MessageType.INFO, null)
+            .createBalloon()
+            .show(
+                RelativePoint.fromScreen(actualPosition),
+                Balloon.Position.below
+            )
     }
 }
