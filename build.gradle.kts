ec00b56bf1953298b97cefcd10c15fa0bc943834
ezura <yuka.ezura@gmail.com>
2021-08-14 16:54:26 +0900

    Add kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("io.kotest:kotest-runner-junit5:4.6.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.6.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
